9e74090f02a0f5d8c3686095d26992a043a284e1
Neel Patel <neel.patel@metropolia.fi>
2022-02-27 23:44:54 +0200

    - Pre Assignment Completed

--- starter-files/src/components/Page/Page.jsx
+++ starter-files/src/components/Page/Page.jsx
@@ -5,31 +5,33 @@ import Form from '../Form';
 import Error from '../Error';
 import Loader from '../Loader';
 import Forecast from '../Forecast';
+
 import useForecast from '../../hooks/useForecast';
 
 import styles from './Page.module.css';
 
-
 const Page = () => {
-   const{isError, isLoading, forecast, submitRequest} =  useForecast();
+    const { isError, isLoading, forecast, submitRequest } = useForecast();
 
-   const onSubmit = (value) => {
-       submitRequest(value);
-   };
+    const onSubmit = value => {
+        submitRequest(value);
+    };
 
     return (
         <Fragment>
             <Header />
+            {!forecast && (
                 <div className={`${styles.box} position-relative`}>
-                    {/*Form*/}
-                    {!isLoading && <Form submitSearch = {onSubmit}/>}
-                    {/*Error*/}
-                    {isError && <Error message={isError}/>}
-                    {/*Loader*/}
+                    {/* Form */}
+                    {!isLoading && <Form submitSearch={onSubmit} />}
+                    {/* Error */}
+                    {isError && <Error message={isError} />}
+                    {/* Loader */}
                     {isLoading && <Loader />}
-                </div>    
-                    {/*Forecast*/}
-                    {forecast && <Forecast />}
+                </div>
+            )}
+            {/* Forecast */}
+            {forecast && <Forecast forecast={forecast} />}
         </Fragment>
     );
 };
